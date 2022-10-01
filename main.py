8b381298d414b09af5d93987cba099b3098dda59
Dom Eccleston <domeccleston@gmail.com>
2022-10-01 16:38:13 +0100

    switch mysql ssl mode

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ planetscale = MySQLdb.connect(
   user=os.getenv("USERNAME"),
   passwd= os.getenv("PASSWORD"),
   db= os.getenv("DATABASE"),
-  ssl_mode = "VERIFY_IDENTITY",
   ssl      = {
     "ca": "/etc/ssl/cert.pem"
   }
