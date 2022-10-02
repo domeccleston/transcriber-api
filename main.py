866822ebbd0d5e51472c5edf2bdf1e9d15ec14bd
Dom Eccleston <domeccleston@gmail.com>
2022-10-02 17:07:23 +0100

    Clean up Python code and add API key protection

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ load_dotenv()
 from flask import Flask, request, abort
 import MySQLdb
 import os
-import validators
 import whisper
 import youtube_dl
 
@@ -18,7 +17,6 @@ planetscale = MySQLdb.connect(
   ssl = 0
 )
 
-
 app = Flask(__name__)
 
 class DownloadLogger(object):
@@ -31,18 +29,13 @@ class DownloadLogger(object):
     def error(self, msg):
         print(msg)
 
-result = ''
-
-def hook(arg):
-    print(arg)
-
 class YoutubeManager:
     def __init__(self, url):
         self.url = url
         self.downloading = False
         self.result_url = ''
         self.result_text = ''
-        self.model = whisper.load_model('base.en')
+        self.model = whisper.load_model('small.en')
         self.ydl_opts = {
         'format': 'bestaudio/best',
         'postprocessors': [{
@@ -68,49 +61,30 @@ class YoutubeManager:
             self.result_url = dl['filename']
             transcript = self.model.transcribe(self.result_url, verbose=True, language='en', fp16=False)
             self.result_text = transcript['text']
-            
-
-class DownloadLogger(object):
-    def debug(self, msg):
-        pass
-
-    def warning(self, msg):
-        pass
-
-    def error(self, msg):
-        print(msg)
-
-def on_finished(dl):
-    if dl['status'] == 'finished':
-        print('Done downloading, now converting...')
-
-def is_valid_url(url):
-    return validators.url(url)
-
-def is_yt_url(url):
-    return url.find('youtube') > -1
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'Transcriber API'
 
 @app.route('/download', methods=['GET', 'POST'])
 def download():
+    try:
+        token = args['token']
+        if token != os.getenv('TRANSCRIPT_API_TOKEN'):
+            return abort(401)
+    except KeyError:
+        return abort(401)
     args = request.args
     url = args['video']
     youtube_manager = YoutubeManager(url)
     youtube_manager.start_download()
-    cursor = planetscale.cursor()
-    insert_sql = "INSERT INTO Transcriptions (url, length, upvotes, transcript) VALUES (%s, %s, %s, %s);"
-    insert_data = (url, str(len(youtube_manager.result_text)), 0, youtube_manager.result_text)
-    cursor.execute(insert_sql, insert_data)
-
-    planetscale.commit()
-
-    for row in cursor:
-        print(row)
-    
-    return 'done'
+    with planetscale.cursor() as cursor:
+        insert_sql = "INSERT INTO Transcriptions (url, length, upvotes, transcript) VALUES (%s, %s, %s, %s);"
+        insert_data = (url, str(len(youtube_manager.result_text)), 0, youtube_manager.result_text)
+        cursor.execute(insert_sql, insert_data)
+        planetscale.commit()
+        cursor.close()
+    return youtube_manager.result_text
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
